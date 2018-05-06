d88f6b912e588a14f5ae1debf779e11777308695
umar-bin-khalid <umar.ubk@gmail.com>
2018-05-06 20:30:32 +0500

    Updations so far in the fyp
    This is my final year project at Punjab University College of Information Technology.
    
    The Project name is Estate Merkez.

--- simplesocial/driver.py
+++ simplesocial/driver.py
@@ -4,12 +4,12 @@ import ZameenScraper
 
 
 
-def mypythonfunction(minimum,maximum,yourpurpose):
+def mypythonfunction(loc,minimum,maximum,yourpurpose):
 
-    zameen_listings = ZameenScraper.Zameen(location_link = 'https://beta.zameen.com/Homes/Lahore_Johar_Town-93-1.html',
-                                                                           price_min = minimum,
-                                                                           price_max = maximum,
-                                                                           purpose = yourpurpose,
-                                                                           page = 1).getListings()
+    zameen_listings = ZameenScraper.Zameen(location_link = loc,
+                                            price_min = minimum,
+                                            price_max = maximum,
+                                            purpose = yourpurpose,
+                                            page = 1).getListings()
 
     return zameen_listings
--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -3,11 +3,25 @@ from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.shortcuts import render
 import driver
+import json
 
 def request_page(request):
+    loc = str(request.GET.get('locations'))
+
+    with open('zameenlocations.json') as f:
+        data = json.load(f)
+        final = data[loc]["link"]
+
     arr = {}
+    pur = str(request.GET.get('mytextbox'))
+
+    if( pur == "Buy"):
+        purpose = 0
+    if(pur == "Rent"):
+        purpose = 1
+
     if(request.GET.get('mybtn')):
-        arr = driver.mypythonfunction( int(request.GET.get('myminbox')),int(request.GET.get('mymaxbox')),int(request.GET.get('mytextbox')) )
+        arr = driver.mypythonfunction( str(final),int(request.GET.get('myminbox')),int(request.GET.get('mymaxbox')), purpose )
         res = {'house_records':arr}
 
     return render(request,'home.html',context=res)
