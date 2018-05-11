4910116e2389acd376e1acf1d3de41248c79ce32
umar-bin-khalid <umar.ubk@gmail.com>
2018-05-11 15:18:59 +0500

    fixed bugs
    some fixes

--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -1,30 +1,38 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from requests.exceptions import ConnectionError
 import driver
 import json
 
 def request_page(request):
-    loc = str(request.GET.get('locations'))
+    try:
 
-    with open('zameenlocations.json') as f:
-        data = json.load(f)
-        final = data[loc]["link"]
+        loc = str(request.GET.get('locations'))
 
-    arr = {}
-    pur = str(request.GET.get('mytextbox'))
+        with open('zameenlocations.json') as f:
+            data = json.load(f)
+            final = data[loc]["link"]
 
-    if( pur == "Buy"):
-        purpose = 0
-    if(pur == "Rent"):
-        purpose = 1
+        arr = {}
+        pur = str(request.GET.get('mytextbox'))
 
-    if(request.GET.get('mybtn')):
-        arr = driver.mypythonfunction( str(final),int(request.GET.get('myminbox')),int(request.GET.get('mymaxbox')), purpose )
-        res = {'house_records':arr}
+        if( pur == "Buy"):
+            purpose = 0
+        if(pur == "Rent"):
+            purpose = 1
 
-    return render(request,'home.html',context=res)
+        if(request.GET.get('mybtn')):
+            arr = driver.mypythonfunction( str(final),int(request.GET.get('myminbox')),int(request.GET.get('mymaxbox')), purpose )
+            res = {'house_records':arr}
+
+        return render(request,'home.html',context=res)
+    except ConnectionError as e:
+        # This is the correct syntax
+        print (e)
+        return HttpResponse("No internet Connection")
+
 
 class TestPage(TemplateView):
     template_name = 'test.html'
